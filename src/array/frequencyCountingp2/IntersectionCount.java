f5c68c159ed6409cb25d002bdcafe9789ca2ee9c
sameer-kumar04 <whoissameer04@gmail.com>
2026-07-26 12:46:27 +0530

    Done Pattern2 excpet the ANAGRAM question

---
+++ src/array/frequencyCountingp2/IntersectionCount.java
@@ -0,0 +1,30 @@
+package array.frequencyCountingp2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class IntersectionCount {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 2, 3};
+        int[] arr2 = {2, 2, 3, 5};
+
+        System.out.println(findNumberOfIntersectionElements(arr1,arr2));
+    }
+
+    public static int findNumberOfIntersectionElements(int[] arr1, int[] arr2) {
+        Set<Integer> set = new HashSet<>();
+        int count = 0;
+        for ( int num : arr1) {
+            set.add(num);
+        }
+
+        for ( int num : arr2) {
+            if ( set.contains(num) ) {
+                count++;
+                set.remove(num);
+            }
+        }
+
+        return count;
+    }
+}
---
+++ src/array/frequencyCountingp2/Union.java
@@ -0,0 +1,34 @@
+package array.frequencyCountingp2;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Union {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3};
+        int[] arr2 = {3, 4, 5};
+
+        System.out.println(findUnion(arr1,arr2));
+    }
+
+    public static List<Integer> findUnion(int[] arr1, int[] arr2) {
+        Set<Integer> set = new HashSet<>();
+        List<Integer> result = new ArrayList<>();
+
+        for ( int num : arr1 ) {
+            set.add(num);
+        }
+
+        for ( int num : arr2) {
+            set.add(num);
+        }
+
+        for ( int num : set) {
+            result.add(num);
+        }
+
+        return result;
+    }
+}
